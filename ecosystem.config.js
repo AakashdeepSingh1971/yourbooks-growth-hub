5b39343085064b8951417be06f7297a8460f321c
Aakashdeep <aakashdeep01971@gmail.com>
2026-02-24 23:40:40 +0530

    added config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,11 @@
 module.exports = {
 	apps: [
 		{
-			name: "yourbooks-next", // name of the project
+			name: "yourbooks-next",
 			script: "node_modules/next/dist/bin/next",
 			args: "start -- -p 3010",
-			cwd: "path to your repo", // path to your repo
 			instances: 1,
 			autorestart: true,
-			watch: false,
 			max_memory_restart: "500M",
 			env: {
 				NODE_ENV: "production",
